7bae4bea776761f1fde1246744aba174ff61c996
sungals91 <sungals911@gmail.com>
2024-10-20 22:11:02 +0900

    플레이어 idle 애니메이션 추가

--- Game.py
+++ Game.py
@@ -1,17 +1,41 @@
 from pico2d import *
 
+class Player:
+    def __init__(self):
+        self.image = load_image('player\Idle.png')
+        self.x, self.y = 400, 300
+        self.frame = 0
+        pass
+    def update(self):
+        self.frame = (self.frame + 1) % 8
+        pass
+    def draw(self):
+        self.image.clip_draw(self.frame * 200, 0, 50, 60, self.x, self.y)
+        pass
 
 
 def reset_world():
     global running
+    global world
+    global player
 
     running = True
+    world = []
+
+    player = Player()
+    world.append(player)
     pass
 
 def update_world():
+    for o in world:
+        o.update()
     pass
 
 def render_world():
+    clear_canvas()
+    for o in world:
+        o.draw()
+    update_canvas()
     pass
 
 def handle_events():
