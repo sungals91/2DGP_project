eebbf95d5f180c6e5d32092d956d3dbcd0cb9ddc
sungals91 <sungals911@gmail.com>
2024-10-20 23:04:42 +0900

    플레이어 캐릭터 이동만 구현
    추후 상태머신을 배운 후 애니메이션까지 추가 구현예정

--- Game.py
+++ Game.py
@@ -2,16 +2,41 @@ from pico2d import *
 
 class Player:
     def __init__(self):
-        self.image = load_image('image\player.png')
+        self.image = load_image('image\\player.png')
         self.x, self.y = 400, 300
+        self.dir_x, self.dir_y = 0,0
         self.frame = 0
         pass
     def update(self):
         self.frame = (self.frame + 1) % 8
-        pass
+        self.x += self.dir_x * 5
+        self.y += self.dir_y * 5
+    pass
     def draw(self):
         self.image.clip_draw(self.frame * 200, 0, 50, 60, self.x, self.y)
         pass
+
+    def handle_event(self, event):
+
+        if event.type == SDL_KEYDOWN:
+            if event.key == SDLK_RIGHT:
+                self.dir_x += 1
+            elif event.key == SDLK_LEFT:
+                self.dir_x -= 1
+            elif event.key == SDLK_UP:
+                self.dir_y += 1
+            elif event.key == SDLK_DOWN:
+                self.dir_y -= 1
+
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                self.dir_x -= 1
+            elif event.key == SDLK_LEFT:
+                self.dir_x += 1
+            elif event.key == SDLK_UP:
+                self.dir_y -= 1
+            elif event.key == SDLK_DOWN:
+                self.dir_y += 1
 
 
 def reset_world():
@@ -40,12 +65,15 @@ def render_world():
 
 def handle_events():
     global running
+
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            running = False
+        elif event.type in (SDL_KEYDOWN, SDL_KEYUP):
+            player.handle_event(event)
+            if event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+                running = False
     pass
 
 open_canvas()
