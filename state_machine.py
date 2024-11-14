9f5052300a551b1357402c1b5d174a8d606a220a
sungals91 <sungals911@gmail.com>
2024-11-14 22:09:16 +0900

    방향키 이벤트 체크 함수 추가

--- state_machine.py
+++ state_machine.py
@@ -1,4 +1,20 @@
+from sdl2 import SDL_KEYDOWN, SDL_KEYUP, SDLK_RIGHT, SDLK_LEFT
 
+
+def right_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_RIGHT
+
+
+def right_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_RIGHT
+
+
+def left_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_LEFT
+
+
+def left_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_LEFT
 
 class StateMachine:
     def __init__(self, o):
