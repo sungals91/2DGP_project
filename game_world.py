fa3b78bcd2a50bc5ab995b2703b4b78d9760e3ab
sungals91 <sungals911@gmail.com>
2024-11-14 22:29:17 +0900

    게임 월드 오류 수정

--- game_world.py
+++ game_world.py
@@ -1,26 +1,32 @@
-objects = [[] for _ in range(4)]
-
+world = [[] for _ in range(4)]
 
 def add_object(o, depth = 0):
-    objects[depth].append(o)
+    world[depth].append(o)
+
+def add_objects(ol, depth = 0):
+    world[depth] += ol
 
 
 def update():
-    for layer in objects:
+    for layer in world:
         for o in layer:
             o.update()
 
 
 def render():
-    for layer in objects:
+    for layer in world:
         for o in layer:
             o.draw()
 
 
 def remove_object(o):
-    for layer in objects:
+    for layer in world:
         if o in layer:
             layer.remove(o)
             return
-
     raise ValueError('Cannot delete non existing object')
+
+
+def clear():
+    for layer in world:
+        layer.clear()
