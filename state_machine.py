1d42f89f1de25bf5cb82b4b60c58f6c88172b1fa
sungals91 <sungals911@gmail.com>
2024-11-14 22:29:37 +0900

    스테이트 머신 기능 추가

--- state_machine.py
+++ state_machine.py
@@ -19,17 +19,36 @@ def left_up(e):
 class StateMachine:
     def __init__(self, o):
         self.o = o
-        pass
+        self.event_que = []
 
     def start(self, state):
         self.cur_state = state
-        self.cur_state.enter(self.o)
-        pass
+
+        print(f'Enter into {state}')
+        self.cur_state.enter(self.o, ('START', 0))
+
+    def add_event(self, e):
+        # print(f'    DEBUG: New event {e} added to event Que')
+        self.event_que.append(e)
+
+    def set_transitions(self, transitions):
+        self.transitions = transitions
 
     def update(self):
         self.cur_state.do(self.o)
-        pass
+        if self.event_que:
+            event = self.event_que.pop(0)
+            self.handle_event(event)
 
     def draw(self):
         self.cur_state.draw(self.o)
-        pass
+
+    def handle_event(self, e):
+        for event, next_state in self.transitions[self.cur_state].items():
+            if event(e):
+                print(f'Exit from {self.cur_state}')
+                self.cur_state.exit(self.o, e)
+                self.cur_state = next_state
+                print(f'Enter into {self.cur_state}')
+                self.cur_state.enter(self.o, e)
+                return
